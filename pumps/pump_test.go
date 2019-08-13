02d7742da9438aa706fec2f23ed48ca70eaeb41b
abhirashi <41231350+abhirashi@users.noreply.github.com>
2019-08-13 23:07:02 +0530

    Issue #126: Enhance the log in case pump not found or connected (#129)
    This contribution is related to Issue# 126, that we were facing whenever we saw failure to connect to a source while creating a pump. Please have a look on the changes.

---
+++ pumps/pump_test.go
@@ -0,0 +1,22 @@
+package pumps
+
+import (
+	"testing"
+)
+
+func TestGetPumpByName(t *testing.T) {
+
+	name := "dummy"
+	pmpType, err := GetPumpByName(name)
+
+	if err != nil || pmpType == nil {
+		t.Fail()
+	}
+
+	name2 := "xyz"
+	pmpType2, err2 := GetPumpByName(name2)
+
+	if err2 == nil || pmpType2 != nil {
+		t.Fail()
+	}
+}
