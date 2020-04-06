8a8cbe9b2e3ac6a0625152d9b41c7d1d77b488ad
Alok G Singh <alephnull@hcoop.net>
2020-04-07 00:18:56 +0530

    Implement debian origin signing.

--- utils/dist_build.sh
+++ utils/dist_build.sh
@@ -9,8 +9,18 @@ echo "Set version number"
 : ${VERSION:=$(perl -n -e'/v(\d+).(\d+).(\d+)/'' && print "$1\.$2\.$3"' version.go)}
 
 if [ $BUILDPKGS == "1" ]; then
+    echo Configuring gpg-agent-config to accept a passphrase
+    mkdir ~/.gnupg && chmod 700 ~/.gnupg
+    cat >> ~/.gnupg/gpg-agent.conf <<EOF
+allow-preset-passphrase
+debug-level expert
+log-file /tmp/gpg-agent.log
+EOF
+    gpg-connect-agent reloadagent /bye
+
     echo "Importing signing key"
-    gpg --list-keys | grep -w $SIGNKEY && echo "Key exists" || gpg --batch --import $BUILDTOOLSDIR/tyk.io.rpm.signing.key
+    gpg --list-keys | grep -w $SIGNKEY && echo "Key exists" || gpg --batch --import $BUILDTOOLSDIR/tyk.io.signing.key
+    bash $BUILDTOOLSDIR/unlock-agent.sh $SIGNKEY
 fi
 
 DESCRIPTION="Tyk Pump to move analytics data from Redis to any supported back end"
@@ -74,9 +84,17 @@ do
         echo "Building $arch packages"
         fpm "${FPMCOMMON[@]}" -a $arch -t deb --deb-user tyk --deb-group tyk ./=/opt/tyk-pump
         fpm "${FPMCOMMON[@]}" "${FPMRPM[@]}" -a $arch -t rpm --rpm-user tyk --rpm-group tyk  ./=/opt/tyk-pump
+    fi
 
-        rpmName="tyk-pump-$VERSION-1.${arch/amd64/x86_64}.rpm"
+    if [ $SIGNPKGS == "1" ]; then
         echo "Signing $arch RPM"
-        $BUILDTOOLSDIR/rpm-sign.sh $rpmName
+        rpm --define "%_gpg_name Team Tyk (package signing) <team@tyk.io>" \
+            --define "%__gpg /usr/bin/gpg" \
+            --addsign *.rpm || (cat /tmp/gpg-agent.log; exit 1)
+        echo "Signing $arch DEB"
+        for i in *.deb
+        do
+            dpkg-sig --sign builder -k $SIGNKEY $i || (cat /tmp/gpg-agent.log; exit 1)
+        done
     fi
 done
