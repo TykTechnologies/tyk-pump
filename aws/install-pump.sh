787c35f5735b753bc4480ea2fd319f861f1b0e64
Alok G Singh <alephnull@hcoop.net>
2021-04-24 00:02:48 +0530

    More missing AWS config files

--- aws/install-pump.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -x
-
-curl -s https://packagecloud.io/install/repositories/tyk/tyk-pump/script.rpm.sh | sudo bash
-sudo yum install -y pygpgme yum-utils wget tyk-pump-$TYK_PUMP_VERSION
-
-sudo rm -f /home/ec2-user/.ssh/authorized_keys
-sudo rm -f /root/.ssh/authorized_keys
