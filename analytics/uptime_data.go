2e6c86b68c2aec35db2354ec035689ad81837969
Leonid Bugaev <leonsbox@gmail.com>
2021-08-04 14:01:41 +0300

    SQL Pump, SQL Aggregate Pump and Uptime SQL Pump (#314)
    ## Description
    Will add soon.
    Basic configuration:
    ```
         "sql": {
                "name": "sql",
                "meta": {
                   "type": "sqlite",
    	       "connection_string": "/tmp/test.db"
    	    }
            },
            "sql_aggregate": {
                "name": "sql_aggregate",
                "meta": {
                   "type": "sqlite",
    	       "connection_string": "/tmp/test.db"
    	    }
            }
    ```
    
    Should be used with https://github.com/TykTechnologies/tyk-analytics/pull/2137
    
    Example to fill with data.
    ```
    ./tyk-pump -c ./pump.conf --demo 588b52eab275ff0001cc7476 --demo-api a47f35f411f14c837b99e99519e2432c --demo-api-version Default
    ```

--- analytics/uptime_data.go
+++ analytics/uptime_data.go
@@ -1,20 +1,265 @@
 package analytics
 
-import "time"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"gorm.io/gorm"
+
+	"github.com/fatih/structs"
+)
+
+const UptimeSQLTable = "tyk_uptime_analytics"
 
 type UptimeReportData struct {
-	URL          string
-	RequestTime  int64
-	ResponseCode int
-	TCPError     bool
-	ServerError  bool
-	Day          int
-	Month        time.Month
-	Year         int
-	Hour         int
-	Minute       int
-	TimeStamp    time.Time
-	ExpireAt     time.Time `bson:"expireAt" json:"expireAt"`
-	APIID        string
-	OrgID        string
+	URL          string     `json:"url"`
+	RequestTime  int64      `json:"request_time"`
+	ResponseCode int        `json:"response_code"`
+	TCPError     bool       `json:"tcp_error"`
+	ServerError  bool       `json:"server_error"`
+	Day          int        `json:"day"`
+	Month        time.Month `json:"month"`
+	Year         int        `json:"year"`
+	Hour         int        `json:"hour"`
+	Minute       int        `json:"minute"`
+	TimeStamp    time.Time  `json:"timestamp"`
+	ExpireAt     time.Time  `bson:"expireAt"`
+	APIID        string     `json:"api_id"`
+	OrgID        string     `json:"org_id"`
+}
+
+type UptimeReportAggregateSQL struct {
+	ID string `gorm:"primaryKey"`
+
+	Counter `json:"counter" gorm:"embedded"`
+
+	TimeStamp      int64  `json:"timestamp" gorm:"index:dimension, priority:1"`
+	OrgID          string `json:"org_id" gorm:"index:dimension, priority:2"`
+	Dimension      string `json:"dimension" gorm:"index:dimension, priority:3"`
+	DimensionValue string `json:"dimension_value" gorm:"index:dimension, priority:4"`
+
+	Code `json:"code" gorm:"embedded"`
+}
+
+func (a *UptimeReportAggregateSQL) TableName() string {
+	return UptimeSQLTable
+}
+
+func (a UptimeReportAggregateSQL) GetAssignments(tableName string) map[string]interface{} {
+	assignments := make(map[string]interface{})
+
+	baseFields := structs.Fields(a.Code)
+	for _, field := range baseFields {
+		jsonTag := field.Tag("json")
+		colName := "code_" + jsonTag
+		if !field.IsZero() {
+			assignments[colName] = gorm.Expr(tableName + "." + colName + " + " + fmt.Sprint(field.Value()))
+		}
+	}
+
+	fields := structs.Fields(a.Counter)
+	for _, field := range fields {
+		colName := "counter_" + field.Tag("json")
+
+		switch {
+		case strings.Contains(colName, "hits"), strings.Contains(colName, "error"), strings.Contains(colName, "success"):
+			if !field.IsZero() {
+				assignments[colName] = gorm.Expr(tableName + "." + colName + " + " + fmt.Sprint(field.Value()))
+			}
+		case strings.Contains(colName, "total_request_time"):
+			if !field.IsZero() {
+				assignments[colName] = gorm.Expr(tableName + "." + colName + " + " + fmt.Sprint(a.TotalRequestTime))
+			}
+		case strings.Contains(colName, "request_time"):
+			//AVG adding value to another AVG: newAve = ((oldAve*oldNumPoints) + x)/(oldNumPoints+1)
+			if !field.IsZero() {
+				assignments[colName] = gorm.Expr("(" + tableName + ".counter_total_request_time  +" + fmt.Sprintf("%v", a.TotalRequestTime) + ")/( " + tableName + ".counter_hits + " + fmt.Sprintf("%v", a.Hits) + ")")
+			}
+		case strings.Contains(colName, "last_time"):
+			if !field.IsZero() {
+				assignments[colName] = gorm.Expr("'" + a.LastTime.Format("2006-01-02 15:04:05-07:00") + "'")
+			}
+		}
+
+	}
+
+	return assignments
+}
+
+func (u *UptimeReportAggregate) Dimensions() (dimensions []Dimension) {
+	for key, inc := range u.URL {
+		dimensions = append(dimensions, Dimension{"url", key, inc})
+	}
+
+	for key, inc := range u.Errors {
+		dimensions = append(dimensions, Dimension{"errors", key, inc})
+	}
+
+	dimensions = append(dimensions, Dimension{"", "total", &u.Total})
+
+	return
+}
+
+type UptimeReportAggregate struct {
+	TimeStamp time.Time
+	OrgID     string
+	TimeID    struct {
+		Year  int
+		Month int
+		Day   int
+		Hour  int
+	}
+
+	URL    map[string]*Counter
+	Errors map[string]*Counter
+
+	Total Counter
+
+	ExpireAt time.Time `bson:"expireAt" json:"expireAt"`
+	LastTime time.Time
+}
+
+func (u UptimeReportAggregate) New() UptimeReportAggregate {
+	agg := UptimeReportAggregate{}
+
+	agg.URL = make(map[string]*Counter)
+	agg.Errors = make(map[string]*Counter)
+
+	return agg
+}
+
+func AggregateUptimeData(data []UptimeReportData) map[string]UptimeReportAggregate {
+	analyticsPerOrg := make(map[string]UptimeReportAggregate)
+
+	for _, thisV := range data {
+		orgID := thisV.OrgID
+
+		if orgID == "" {
+			continue
+		}
+
+		thisAggregate, found := analyticsPerOrg[orgID]
+
+		if !found {
+			thisAggregate = UptimeReportAggregate{}.New()
+
+			// Set the hourly timestamp & expiry
+			asTime := thisV.TimeStamp
+			thisAggregate.TimeStamp = time.Date(asTime.Year(), asTime.Month(), asTime.Day(), asTime.Hour(), 0, 0, 0, asTime.Location())
+
+			thisAggregate.ExpireAt = thisV.ExpireAt
+			thisAggregate.TimeID.Year = asTime.Year()
+			thisAggregate.TimeID.Month = int(asTime.Month())
+			thisAggregate.TimeID.Day = asTime.Day()
+			thisAggregate.TimeID.Hour = asTime.Hour()
+			thisAggregate.OrgID = orgID
+			thisAggregate.LastTime = thisV.TimeStamp
+			thisAggregate.Total.ErrorMap = make(map[string]int)
+		}
+
+		// Always update the last timestamp
+		thisAggregate.LastTime = thisV.TimeStamp
+
+		// Create the counter for this record
+		var thisCounter Counter
+		if thisV.ResponseCode == -1 {
+			thisCounter = Counter{
+				LastTime: thisV.TimeStamp,
+			}
+			if thisV.URL != "" {
+				c := thisAggregate.URL[thisV.URL]
+				if c == nil {
+					c = &Counter{
+						Identifier: thisV.URL,
+					}
+					thisAggregate.URL[thisV.URL] = c
+				}
+			}
+		} else {
+			thisCounter = Counter{
+				Hits:             1,
+				Success:          0,
+				ErrorTotal:       0,
+				RequestTime:      float64(thisV.RequestTime),
+				TotalRequestTime: float64(thisV.RequestTime),
+				LastTime:         thisV.TimeStamp,
+				ErrorMap:         make(map[string]int),
+			}
+			thisAggregate.Total.Hits++
+			thisAggregate.Total.TotalRequestTime += float64(thisV.RequestTime)
+
+			// We need an initial value
+			thisAggregate.Total.RequestTime = thisAggregate.Total.TotalRequestTime / float64(thisAggregate.Total.Hits)
+			if thisV.ResponseCode >= 400 {
+				thisCounter.ErrorTotal = 1
+				thisCounter.ErrorMap[strconv.Itoa(thisV.ResponseCode)]++
+				thisAggregate.Total.ErrorTotal++
+				thisAggregate.Total.ErrorMap[strconv.Itoa(thisV.ResponseCode)]++
+			}
+
+			if (thisV.ResponseCode < 300) && (thisV.ResponseCode >= 200) {
+				thisCounter.Success = 1
+				thisAggregate.Total.Success++
+				//using the errorMap as ResponseCode Map for SQL purpose
+				thisCounter.ErrorMap[strconv.Itoa(thisV.ResponseCode)]++
+				thisAggregate.Total.ErrorMap[strconv.Itoa(thisV.ResponseCode)]++
+			}
+
+			// Convert to a map (for easy iteration)
+			vAsMap := structs.Map(thisV)
+			for key, value := range vAsMap {
+
+				// Mini function to handle incrementing a specific counter in our object
+				IncrementOrSetUnit := func(c *Counter) *Counter {
+					if c == nil {
+						newCounter := thisCounter
+						newCounter.ErrorMap = make(map[string]int)
+						for k, v := range thisCounter.ErrorMap {
+							newCounter.ErrorMap[k] = v
+						}
+						c = &newCounter
+					} else {
+						c.Hits += thisCounter.Hits
+						c.Success += thisCounter.Success
+						c.ErrorTotal += thisCounter.ErrorTotal
+						for k, v := range thisCounter.ErrorMap {
+							c.ErrorMap[k] += v
+						}
+						c.TotalRequestTime += thisCounter.TotalRequestTime
+						c.RequestTime = c.TotalRequestTime / float64(c.Hits)
+					}
+
+					return c
+				}
+
+				switch key {
+				case "URL":
+					c := IncrementOrSetUnit(thisAggregate.URL[value.(string)])
+					if value.(string) != "" {
+						thisAggregate.URL[value.(string)] = c
+						thisAggregate.URL[value.(string)].Identifier = thisV.URL
+					}
+					break
+				case "ResponseCode":
+					errAsStr := strconv.Itoa(value.(int))
+					if errAsStr != "" {
+						c := IncrementOrSetUnit(thisAggregate.Errors[errAsStr])
+						if c.ErrorTotal > 0 {
+							thisAggregate.Errors[errAsStr] = c
+							thisAggregate.Errors[errAsStr].Identifier = errAsStr
+						}
+					}
+					break
+				}
+			}
+
+		}
+
+		analyticsPerOrg[orgID] = thisAggregate
+
+	}
+
+	return analyticsPerOrg
 }
---
+++ pumps/sql_aggregate.go
@@ -0,0 +1,181 @@
+package pumps
+
+import (
+	"context"
+	"encoding/hex"
+	"fmt"
+
+	"github.com/TykTechnologies/tyk-pump/analytics"
+	"gorm.io/gorm/clause"
+
+	"github.com/mitchellh/mapstructure"
+
+	"gorm.io/gorm"
+	gorm_logger "gorm.io/gorm/logger"
+)
+
+type SQLAggregatePumpConf struct {
+	SQLConf `mapstructure:",squash"`
+
+	EnvPrefix               string   `mapstructure:"meta_env_prefix"`
+	TrackAllPaths           bool     `mapstructure:"track_all_paths"`
+	IgnoreTagPrefixList     []string `mapstructure:"ignore_tag_prefix_list"`
+	ThresholdLenTagList     int      `mapstructure:"threshold_len_tag_list"`
+	StoreAnalyticsPerMinute bool     `mapstructure:"store_analytics_per_minute"`
+	IgnoreAggregationsList  []string `mapstructure:"ignore_aggregations"`
+}
+
+type SQLAggregatePump struct {
+	CommonPumpConfig
+
+	SQLConf *SQLAggregatePumpConf
+
+	db      *gorm.DB
+	dbType  string
+	dialect gorm.Dialector
+}
+
+var SQLAggregatePumpPrefix = "SQL-aggregate-pump"
+var SQLAggregateDefaultENV = PUMPS_ENV_PREFIX + "_SQLAGGREGATE" + PUMPS_ENV_META_PREFIX
+
+func (c *SQLAggregatePump) New() Pump {
+	newPump := SQLAggregatePump{}
+	return &newPump
+}
+
+func (c *SQLAggregatePump) GetName() string {
+	return "SQL Aggregate Pump"
+}
+
+func (c *SQLAggregatePump) GetEnvPrefix() string {
+	return c.SQLConf.EnvPrefix
+}
+
+func (c *SQLAggregatePump) Init(conf interface{}) error {
+	c.SQLConf = &SQLAggregatePumpConf{}
+	c.log = log.WithField("prefix", SQLAggregatePumpPrefix)
+
+	err := mapstructure.Decode(conf, &c.SQLConf)
+	if err != nil {
+		c.log.Error("Failed to decode configuration: ", err)
+		return err
+	}
+
+	processPumpEnvVars(c, c.log, c.SQLConf, SQLAggregateDefaultENV)
+
+	logLevel := gorm_logger.Silent
+
+	switch c.SQLConf.LogLevel {
+	case "debug":
+		logLevel = gorm_logger.Info
+	case "info":
+		logLevel = gorm_logger.Warn
+	case "warning":
+		logLevel = gorm_logger.Error
+	}
+
+	dialect, errDialect := Dialect(&c.SQLConf.SQLConf)
+	if errDialect != nil {
+		c.log.Error(errDialect)
+		return errDialect
+	}
+	db, err := gorm.Open(dialect, &gorm.Config{
+		AutoEmbedd:             true,
+		UseJSONTags:            true,
+		SkipDefaultTransaction: true,
+		Logger:                 gorm_logger.Default.LogMode(logLevel),
+	})
+
+	if err != nil {
+		c.log.Error(err)
+		return err
+	}
+	c.db = db
+	if !c.SQLConf.TableSharding {
+		c.db.Table(analytics.AggregateSQLTable).AutoMigrate(&analytics.SQLAnalyticsRecordAggregate{})
+	}
+
+	c.log.Debug("SQLAggregate Initialized")
+	return nil
+}
+
+// WriteData aggregates and writes the passed data to SQL database. When table sharding is enabled, startIndex and endIndex
+// are found by checking timestamp of the records. The main for loop iterates and finds the index where a new day starts.
+// Then, the data is passed to AggregateData function and written to database day by day on different tables. However,
+// if table sharding is not enabled, the for loop iterates one time and all data is passed at once to the AggregateData
+// function and written to database on single table.
+func (c *SQLAggregatePump) WriteData(ctx context.Context, data []interface{}) error {
+	dataLen := len(data)
+	c.log.Debug("Attempting to write ", dataLen, " records...")
+
+	if dataLen == 0 {
+		return nil
+	}
+
+	startIndex := 0
+	endIndex := dataLen
+	table := ""
+	for i := 0; i <= dataLen; i++ {
+		if c.SQLConf.TableSharding {
+			recDate := data[startIndex].(analytics.AnalyticsRecord).TimeStamp.Format("20060102")
+			var nextRecDate string
+			//if we're on i == dataLen iteration, it means that we're out of index range. We're going to use the last record date.
+			if i == dataLen {
+				nextRecDate = data[dataLen-1].(analytics.AnalyticsRecord).TimeStamp.Format("20060102")
+			} else {
+				nextRecDate = data[i].(analytics.AnalyticsRecord).TimeStamp.Format("20060102")
+
+				//if both dates are equal, we shouldn't write in the table yet.
+				if recDate == nextRecDate {
+					continue
+				}
+			}
+
+			endIndex = i
+
+			table = analytics.AggregateSQLTable + "_" + recDate
+			c.db = c.db.Table(table)
+			if !c.db.Migrator().HasTable(table) {
+				c.db.AutoMigrate(&analytics.SQLAnalyticsRecordAggregate{})
+			}
+		} else {
+			i = dataLen // write all records at once for non-sharded case, stop for loop after 1 iteration
+			table = analytics.AggregateSQLTable
+		}
+
+		analyticsPerOrg := analytics.AggregateData(data[startIndex:endIndex], c.SQLConf.TrackAllPaths, c.SQLConf.IgnoreTagPrefixList, c.SQLConf.StoreAnalyticsPerMinute)
+
+		for orgID, ag := range analyticsPerOrg {
+			for _, d := range ag.Dimensions() {
+				id := fmt.Sprintf("%v", ag.TimeStamp.Unix()) + orgID + d.Name + d.Value
+				uID := hex.EncodeToString([]byte(id))
+
+				rec := analytics.SQLAnalyticsRecordAggregate{
+					ID:             uID,
+					OrgID:          orgID,
+					TimeStamp:      ag.TimeStamp.Unix(),
+					Counter:        *d.Counter,
+					Dimension:      d.Name,
+					DimensionValue: d.Value,
+				}
+				rec.ProcessStatusCodes(rec.Counter.ErrorMap)
+				rec.Counter.ErrorList = nil
+				rec.Counter.ErrorMap = nil
+
+				tx := c.db.WithContext(ctx).Clauses(clause.OnConflict{
+					Columns:   []clause.Column{{Name: "id"}},
+					DoUpdates: clause.Assignments(rec.GetAssignments(table)),
+				}).Create(rec)
+				if tx.Error != nil {
+					return tx.Error
+				}
+			}
+		}
+
+		startIndex = i // next day start index, necessary for sharded case
+	}
+
+	c.log.Info("Purged ", dataLen, " records...")
+
+	return nil
+}
