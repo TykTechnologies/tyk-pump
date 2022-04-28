2c44091556925e16bc7c0faeb5db6e1e408542ef
sredny buitrago <sredny.buitrago@gmail.com>
2022-04-28 13:42:39 +0200

    remove all references to gotiny

--- serializer/serializer.go
+++ serializer/serializer.go
@@ -1,11 +1,8 @@
 package serializer
 
 import (
-	"reflect"
-
 	"github.com/TykTechnologies/tyk-pump/analytics"
 	logger "github.com/TykTechnologies/tyk-pump/logger"
-	"github.com/niubaoshu/gotiny"
 )
 
 var log = logger.GetLogger()
@@ -17,20 +14,10 @@ type AnalyticsSerializer interface {
 }
 
 const MSGP_SERIALIZER = "msgpack"
-const GOTINY_SERIALIZER = "gotiny"
 const PROTOBUF_SERIALIZER = "protobuf"
 
 func NewAnalyticsSerializer(serializerType string) AnalyticsSerializer {
 	switch serializerType {
-	case GOTINY_SERIALIZER:
-		serializer := &GoTinySerializer{}
-
-		recordType := reflect.TypeOf(analytics.AnalyticsRecord{})
-		serializer.encoder = gotiny.NewEncoderWithType(recordType)
-		serializer.decoder = gotiny.NewDecoderWithType(recordType)
-
-		log.Debugf("Using serializer %v for analytics \n", GOTINY_SERIALIZER)
-		return serializer
 	case PROTOBUF_SERIALIZER:
 		serializer := &ProtobufSerializer{}
 		log.Debugf("Using serializer %v for analytics \n", PROTOBUF_SERIALIZER)
