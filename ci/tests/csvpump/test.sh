bdf04bf033c0aae666fc57cc573c4e8c46a79abc
Asutosh <asutosh@tyk.io>
2022-08-08 01:11:55 +0530

    Make gateway image tag optional.

--- ci/tests/csvpump/test.sh
+++ ci/tests/csvpump/test.sh
@@ -1,16 +1,17 @@
 #!/bin/bash  
 
-set -eaxo pipefail
+set -eao pipefail
 
 function usage {
     local progname=$1
     cat <<EOF
 Usage:
 
-    $progname <tag> <gwtag>
+    $progname  tag [gwtag]
 
-    tag - The tag for the pump image, that needs to be tested.
-    gwtag - The  tag for the GW image, that needs to be tested against the pump.
+    tag   -  The tag for the pump image, that needs to be tested.
+    gwtag -  (Optional) The  tag for the GW image, that needs to be tested against the pump.
+             If not given, the default version would be used, which is $DEFAULT_GW_TAG
 
 Brings up the given tyk gw, and tyk pump and checks if the csv pump is
 working properly.
@@ -20,23 +21,39 @@ EOF
     exit 1
 }
 
+function info {
+    local text=$1
+    echo -e "\033[0;36m $text \033[0m"
+}
 
+function warn {
+    local text=$1
+    echo -e "\033[0;31m $text \033[0m"
+}
 
-[[ -z $1 || -z $2 ]] && usage $0
+# The default gateway image that will be pulled and checked against in case
+# no explicit gateway image tag is given.
+DEFAULT_GW_TAG=v4.0.1
+
+[[ -z $1 ]] && usage "$0"
 export tag=$1
 export gwtag=$2
+
+if [[ -z $2 ]]; then
+    warn "No explicit gateway tag given - Will use $DEFAULT_GW_TAG"
+    gwtag=$DEFAULT_GW_TAG
+fi
 
 compose='docker-compose'
 # use the compose client plugin if v2
 [[ $(docker version --format='{{ .Client.Version }}') =~ 20.10 ]] && compose='docker compose'
 
-#create the tmp directory to hold pump data
+#create the tmp directory to hold pump data.
 if [[ -e /tmp/pump-data ]]; then
     rm -rf /tmp/pump-data/*.csv
 else
     mkdir /tmp/pump-data
 fi
-
 
 trap '$compose down' EXIT
 
@@ -49,13 +66,12 @@ curlf() {
     curl --header 'content-type:application/json' -s --show-error "$@"
 }
 
-
 cleanup() {
     rm -f /tmp/pump-data/*.csv
 }
 
 check_gw_status() {
-    echo "Checking Tyk GW status..."
+    info "Checking Tyk GW status..."
     status=$(curlf "${GWBASE}/hello" | jq -r '.status')
     if [ "$status" != "pass" ]
     then
@@ -72,22 +88,21 @@ check_gw_status() {
 # Check if gw is up, if not wait a bit.
 if ! check_gw_status
 then
-    echo "Gateway & gateway redis is not yet up, waiting a bit..."
+    info "Gateway & gateway redis is not yet up, waiting a bit..."
     sleep 10
 fi
 
-
 # Add the test API - keyless APIs are not getting exported when pump is run.
-echo "Adding a test API to the Tyk GW..."
+info "Adding a test API to the Tyk GW..."
 curlf --header "x-tyk-authorization: 352d20ee67be67f6340b4c0605b044b7" \
     -XPOST --data @data/api.authenabled.json ${GWBASE}/tyk/apis
 
 # Add a corresponding key
-echo "Adding a key for the added API..."
+info "Adding a key for the added API..."
 KEY=$(curlf --header "x-tyk-authorization: 352d20ee67be67f6340b4c0605b044b7" -XPOST --data @data/key.json ${GWBASE}/tyk/keys | jq -r '.key')
 
 # Hot reload gateway
-echo "Executing gateway hot reload..."
+info "Executing gateway hot reload..."
 curlf --header "x-tyk-authorization: 352d20ee67be67f6340b4c0605b044b7" \
     ${GWBASE}/tyk/reload/group
 
@@ -95,7 +110,7 @@ curlf --header "x-tyk-authorization: 352d20ee67be67f6340b4c0605b044b7" \
 sleep 2
 
 # Get the key from the previous step and access the API endpoint with the key and a custom user agent.
-echo "Accessing the added API with a custom user agent string..."
+info "Accessing the added API with a custom user agent string..."
 curl -v --header "Authorization: $KEY" --header "User-Agent: HAL9000" \
     "${GWBASE}/test/"
 
@@ -105,9 +120,9 @@ sleep 20
 # Search for our custom  user agent in the csv file.
 if grep "HAL9000" /tmp/pump-data/*.csv
 then
-    echo "CSV Pump test completed successfully.."
+    info "CSV Pump test completed successfully.."
 else
-    echo "CSV pump test failed.."
+    warn "CSV pump test failed.."
     cleanup
     exit 1
 fi
