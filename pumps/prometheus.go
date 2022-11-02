ba9b9277238cba91812a4f43c3cda7b045791999
Tomas Buchaillot <tombuchaillot89@gmail.com>
2022-11-02 12:58:13 +0100

    adding prometheus default env var postfix (#509)
    (cherry picked from commit a4631e82c276f2911eb5d2a397227cd7d6bd4c7c)

--- pumps/prometheus.go
+++ pumps/prometheus.go
@@ -91,7 +91,7 @@ const (
 
 var (
 	prometheusPrefix     = "prometheus-pump"
-	prometheusDefaultENV = PUMPS_ENV_PREFIX + "_PROMETHEUS"
+	prometheusDefaultENV = PUMPS_ENV_PREFIX + "_PROMETHEUS" + PUMPS_ENV_META_PREFIX
 )
 
 var buckets = []float64{1, 2, 5, 7, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 1000, 2000, 5000, 10000, 30000, 60000}
