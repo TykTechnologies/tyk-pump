82f08646d5a462133ac79bd14e5d051544be25f8
mativm02 <matipvp02@gmail.com>
2022-12-02 11:23:56 -0300

    adding unit tests to csv file

---
+++ pumps/csv_test.go
@@ -0,0 +1,240 @@
+package pumps
+
+import (
+	"context"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/TykTechnologies/tyk-pump/analytics/demo"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCSVPump_New(t *testing.T) {
+	type fields struct {
+		csvConf          *CSVConf
+		wroteHeaders     bool
+		CommonPumpConfig CommonPumpConfig
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   Pump
+	}{
+		{
+			name: "TestCSVPump_New",
+			fields: fields{
+				csvConf: &CSVConf{},
+				CommonPumpConfig: CommonPumpConfig{
+					log: log.WithField("prefix", csvPrefix),
+				},
+			},
+			want: &CSVPump{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &CSVPump{
+				csvConf:          tt.fields.csvConf,
+				wroteHeaders:     tt.fields.wroteHeaders,
+				CommonPumpConfig: tt.fields.CommonPumpConfig,
+			}
+			if got := c.New(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CSVPump.New() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCSVPump_GetName(t *testing.T) {
+	type fields struct {
+		csvConf          *CSVConf
+		wroteHeaders     bool
+		CommonPumpConfig CommonPumpConfig
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "TestCSVPump_GetName",
+			fields: fields{
+				csvConf: &CSVConf{},
+				CommonPumpConfig: CommonPumpConfig{
+					log: log.WithField("prefix", csvPrefix),
+				},
+			},
+			want: "CSV Pump",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &CSVPump{
+				csvConf:          tt.fields.csvConf,
+				wroteHeaders:     tt.fields.wroteHeaders,
+				CommonPumpConfig: tt.fields.CommonPumpConfig,
+			}
+			if got := c.GetName(); got != tt.want {
+				t.Errorf("CSVPump.GetName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCSVPump_Init(t *testing.T) {
+	type fields struct {
+		csvConf          *CSVConf
+		wroteHeaders     bool
+		CommonPumpConfig CommonPumpConfig
+	}
+	type args struct {
+		conf interface{}
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "TestCSVPump_Init",
+			args: args{
+				conf: &CSVConf{
+					CSVDir: "testingDirectory",
+				},
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &CSVPump{
+				csvConf:          tt.fields.csvConf,
+				wroteHeaders:     tt.fields.wroteHeaders,
+				CommonPumpConfig: tt.fields.CommonPumpConfig,
+			}
+			if err := c.Init(tt.args.conf); (err != nil) != tt.wantErr {
+				t.Errorf("CSVPump.Init() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if tt.wantErr {
+				return
+			}
+
+			defer os.Remove(c.csvConf.CSVDir)
+
+			_, err := os.Stat(c.csvConf.CSVDir)
+			if err != nil {
+				t.Errorf("CSVPump.Init() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+		})
+	}
+}
+
+func TestCSVPump_WriteData(t *testing.T) {
+	type fields struct {
+		csvConf      *CSVConf
+		wroteHeaders bool
+	}
+	type args struct {
+		numberOfRecords int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "writing 1 record",
+			fields: fields{
+				csvConf: &CSVConf{
+					CSVDir: "testingDirectory",
+				},
+			},
+			args: args{
+				numberOfRecords: 1,
+			},
+		},
+		{
+			name: "writing 10 records",
+			fields: fields{
+				csvConf: &CSVConf{
+					CSVDir: "testingDirectory",
+				},
+			},
+			args: args{
+				numberOfRecords: 10,
+			},
+		},
+		{
+			name: "trying to write invalid records",
+			fields: fields{
+				csvConf: &CSVConf{
+					CSVDir: "testingDirectory",
+				},
+			},
+			args: args{
+				numberOfRecords: 0,
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			// creating and initializing csv pump
+			c := &CSVPump{
+				csvConf:      tt.fields.csvConf,
+				wroteHeaders: tt.fields.wroteHeaders,
+			}
+			c.Init(tt.fields.csvConf)
+			// when init, a directory is created, so we need to remove it after the test
+			defer os.RemoveAll(c.csvConf.CSVDir)
+			// generating random data to write in the csv file
+			var records []interface{}
+			if !tt.wantErr {
+				for i := 0; i < tt.args.numberOfRecords; i++ {
+					records = append(records, demo.GenerateRandomAnalyticRecord("orgid", false))
+				}
+			} else {
+				records = append(records, "invalid record")
+			}
+			// writing data
+			if err := c.WriteData(context.Background(), records); (err != nil) != tt.wantErr {
+				t.Errorf("CSVPump.WriteData() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			// getting the file name
+			curtime := time.Now()
+			fname := fmt.Sprintf("%d-%s-%d-%d.csv", curtime.Year(), curtime.Month().String(), curtime.Day(), curtime.Hour())
+
+			// checking if the file exists
+			openfile, err := os.Open("./testingDirectory/" + fname)
+			if err != nil {
+				t.Error(err)
+			}
+			defer openfile.Close()
+			filedata, err := csv.NewReader(openfile).ReadAll()
+			if err != nil {
+				t.Error(err)
+			}
+
+			// checking if the file contains the right number of records (number of records +1 because of the header)
+			totalRows := len(filedata)
+			if tt.wantErr {
+				assert.Equal(t, tt.args.numberOfRecords, totalRows)
+			} else {
+				assert.Equal(t, tt.args.numberOfRecords+1, totalRows)
+			}
+
+		})
+	}
+}
