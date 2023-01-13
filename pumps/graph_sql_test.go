9c744d0db6790164b3c5c1bd35aeed55e871271a
Kofo Okesola <okesolakofo@gmail.com>
2023-01-13 13:18:35 +0100

    add write data tests

--- pumps/graph_sql_test.go
+++ pumps/graph_sql_test.go
@@ -1,7 +1,11 @@
 package pumps
 
 import (
+	"context"
+	"encoding/base64"
 	"github.com/TykTechnologies/tyk-pump/analytics"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -53,16 +57,35 @@ func TestGraphSQLPump_Init(t *testing.T) {
 	})
 }
 
+func convToBase64(raw string) string {
+	return base64.StdEncoding.EncodeToString([]byte(raw))
+}
+
 func TestGraphSQLPump_WriteData(t *testing.T) {
+	conf := SQLConf{
+		Type:             "sqlite",
+		ConnectionString: "",
+		TableName:        "test-table",
+	}
+	pump := &GraphSQLPump{}
+	assert.NoError(t, pump.Init(conf))
+
 	type customRecord struct {
 		isHttp       bool
 		tags         []string
+		response     string
 		responseCode int
 	}
+	type expectedResponse struct {
+		types         map[string][]string
+		operationType string
+	}
+
 	testCases := []struct {
-		name           string
-		recordModifier func(record analytics.AnalyticsRecord) analytics.AnalyticsRecord
-		records        []customRecord
+		name      string
+		records   []customRecord
+		responses []expectedResponse
+		hasError  bool
 	}{
 		{
 			name: "default case",
@@ -71,27 +94,66 @@ func TestGraphSQLPump_WriteData(t *testing.T) {
 					isHttp:       false,
 					tags:         []string{analytics.PredefinedTagGraphAnalytics},
 					responseCode: 200,
+					response:     rawGQLResponse,
 				},
 			},
+			responses: []expectedResponse{
+				{
+					types: map[string][]string{
+						"Country": {"code"},
+					},
+					operationType: "Query",
+				},
+			},
+			hasError: false,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			records := make([]interface{}, 0)
-			for _, cr := range tc.records {
+			expectedResponses := make([]analytics.GraphRecord, 0)
+			for _, item := range tc.records {
 				r := analytics.AnalyticsRecord{
 					APIName: "Test API",
 					Path:    "POST",
-					//RawRequest:  base64.StdEncoding.EncodeToString([]byte(cr.rawRequest)),
-					//RawResponse: base64.StdEncoding.EncodeToString([]byte(cr.rawResponse)),
-					//ApiSchema:   base64.StdEncoding.EncodeToString([]byte(cr.schema)),
-					Tags: cr.tags,
+					Tags:    item.tags,
 				}
-				if cr.responseCode != 0 {
-					r.ResponseCode = cr.responseCode
+				if !item.isHttp {
+					r.RawRequest = convToBase64(rawGQLRequest)
+					r.ApiSchema = convToBase64(schema)
+				} else {
+					r.RawRequest = convToBase64(rawHTTPReq)
+					r.RawResponse = convToBase64(rawHTTPResponse)
+				}
+				r.RawResponse = item.response
+				if item.responseCode != 0 {
+					r.ResponseCode = item.responseCode
 				}
 				records = append(records, r)
+			}
+
+			for _, item := range tc.responses {
+				r := analytics.GraphRecord{
+					Types:         item.types,
+					OperationType: item.operationType,
+					Errors:        []analytics.GraphError{},
+				}
+				expectedResponses = append(expectedResponses, r)
+			}
+
+			err := pump.WriteData(context.Background(), records)
+			if !tc.hasError {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+			}
+
+			var resultRecords []analytics.GraphRecord
+			pump.db.Table(conf.TableName).Find(&resultRecords)
+			assert.Equal(t, len(tc.responses), len(resultRecords), "responses count do no match")
+			if diff := cmp.Diff(expectedResponses, resultRecords, cmpopts.IgnoreFields(analytics.GraphRecord{}, "AnalyticsRecord")); diff != "" {
+				t.Error(diff)
 			}
 		})
 	}
